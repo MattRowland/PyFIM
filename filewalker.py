b274340210e656e5c6c4cc3e6b17d086ae386078
MattRowland <MattRowland@users.noreply.github.com>
2019-05-04 19:08:53 -0600

    Detect Duplicate
    Changed styleing
    added duplictate detection

--- filewalker.py
+++ filewalker.py
@@ -2,15 +2,18 @@ import os
 import hashlib
 import progress
 
-def collectPaths(basePath):
+def collect_paths(basePath):
+    print("Walking path...")
     filepaths = []
     for base, dirs, files in os.walk(basePath):
         for name in files:
             filepaths.append(os.path.join(base, name))
     return filepaths
 
-def calculateHash(filepath):
+def calculate_hash(filepath):
+    print("Calculating hashes...")
     hasher = hashlib.sha256()
+    print()
     try:
         with open(filepath, "rb") as f:
             for block in iter(lambda: f.read(4096), b""):
@@ -19,15 +22,36 @@ def calculateHash(filepath):
         print("Could not process", filepath, ":", e)
     return hasher.hexdigest()
 
-def calculateHashes(filespaths):
+def calculate_hashes(filespaths):
     fileInfos = []
     total = len(filespaths)
     i = 0
     for filepath in filespaths:
         i += 1
         progress.progress(i, total, "Calculating Hashes")
-        fileInfos.append({"filepath":filepath,"digest":calculateHash(filepath)})
+        fileInfos.append({"filepath":filepath,"digest":calculate_hash(filepath)})
     return fileInfos
 
-def detectDuplicates():
-    
+def detect_duplicates(fileInfos):
+    dictionary = {}
+    for fileInfo in fileInfos:
+        print(fileInfo)
+        if fileInfo["digest"] in dictionary:
+            dictionary[fileInfo["digest"]].append(fileInfo["filepath"])
+        else:
+            dictionary[fileInfo["digest"]] = [fileInfo["filepath"]]
+
+    duplicateItems = []
+    for digest, filepaths in dictionary.items():
+        if len(filepaths) > 1:
+            duplicateItems.append({"digest":digest, "filepaths":filepaths})
+
+    if len(duplicateItems) == 0:
+        print("No Duplicates!")
+    else:
+        print("Duplicate Items")
+        for item in duplicateItems:
+            print("-"*74)
+            print("--- ",item["digest"].upper()," ---")
+            print("\n".join(item["filepaths"]))
+            print("-"*74)
